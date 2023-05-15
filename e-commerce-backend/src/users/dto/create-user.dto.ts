5404300559eb3a4411701f8cd9e518287ec38207
fg-61 <fatihgoren61@gmail.com>
2023-05-15 12:09:50 +0300

    user crud with pg

--- e-commerce-backend/src/users/dto/create-user.dto.ts
+++ e-commerce-backend/src/users/dto/create-user.dto.ts
@@ -1,1 +1,15 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, MinLength } from "class-validator";
+
+export class CreateUserDto {
+    @IsNotEmpty()
+    @MinLength(3)
+    username: string;
+
+    @IsNotEmpty()
+    @MinLength(8)
+    password: string;
+
+    @IsNotEmpty()
+    @IsEmail()
+    email: string;
+}
--- e-commerce-backend/src/users/users.service.ts
+++ e-commerce-backend/src/users/users.service.ts
@@ -1,15 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) { }
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    const newUser = this.userRepository.create(createUserDto);
+    return this.userRepository.save(newUser);
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.userRepository.find({
+      select: {
+        id: true,
+        username: true,
+        password: true,
+        email: true
+      },
+    })
   }
 
   findOne(id: number) {
